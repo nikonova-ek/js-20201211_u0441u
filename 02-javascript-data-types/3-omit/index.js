ca709eae44f21d6bc2a2df31d230273a98729f30
Ekaterina Nikonova <u0441u@gmail.com>
2020-12-17 14:50:54 +0100

    3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  let array = Object.entries(obj);
+  array = array.filter(function(element){
+  let key = element[0];
+  return !fields.includes(key);});
 
-};
+  return Object.fromEntries(array);}
+
+
